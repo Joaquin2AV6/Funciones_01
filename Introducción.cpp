6c7806aeb82a258da49e4bd29d813c657b85dfb6
Joaquin Arroniz Rosas <79723164+Joaquin2AV6@users.noreply.github.com>
2021-03-03 16:43:19 -0600

    Agregando mi segunda tarea a mi repositorio.

---
+++ Introducción.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main ()
+{
+    int var1;
+    float var2;
+    char var3;
+
+    cout<<"PRESENTANDO MI PRIMER PROGRAMA EN C++"<<endl<<endl;
+
+        cout<<"Digite un numero : ";
+        cin>>var1;
+        cout<<"El numero es : "<<var1<<endl<<endl;
+
+        cout<<"Digite un flotante : ";
+        cin>>var2;
+        cout<<"El numero es : "<<var2<<endl<<endl;
+
+        cout<<"Introduzca una letra : ";
+        cin>>var3;
+        cout<<"La letra es : "<<var3<<endl<<endl;
+
+        getch();
+        return 0;
+
+
+}
